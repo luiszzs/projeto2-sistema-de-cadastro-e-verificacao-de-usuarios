d4fddf2b1e5f56770fa8128413fbf6b7db66c8f1
Luis Guilherme <luis.gomes76@aluno.ce.gov.br>
2025-08-22 19:40:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //Cadastro de usuario
-alert("Olá, vamos realizar seu Cadastro?")
 const nome = prompt("Digite seu nome:")
 let idade = parseInt(prompt("digite sua idade"))
 //Verifica a idade do usuario
@@ -9,7 +8,6 @@ if(idade < 13) {
 //perguntar a senha
  let senha = prompt("Crie sua senha")
 //Login
-alert("Faça seu login")
 let aprovNome = prompt("Digite seu nome novamente:")
 let aprovSenha = prompt("Digite sua senha novamente:")
 //Verifica se a senha e o nome estão corretos
@@ -18,4 +16,5 @@ alert(`Login realizado com sucesso! Bem-vindo, ${nome}.`)
 }else {
     alert(`Nome ou senha incorretos. Tente novamente.`)
 }
+
 }
